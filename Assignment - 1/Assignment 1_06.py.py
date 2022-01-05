396647149769844fe89bf899db6410873285108b
Shivani Mohite <92145816+2311shivani@users.noreply.github.com>
2022-01-05 11:21:13 +0530

    Add files via upload

---
+++ Assignment - 1/Assignment 1_06.py.py
@@ -0,0 +1,19 @@
+#6.Write a program which accept number from user and check whether that number is positive or negative or zero.
+
+def printnumber(n):
+        
+          if n > 0:
+                    print("Number is positive")
+          elif n < 0:
+                    print("Number is  negative")
+          elif n == 0:
+                    print("Number is  zero")
+
+n = int(input(" Enter the  number "))
+printnumber(n)
+
+
+
+
+        
+
---
+++ Assignment - 1/Assignment 1_10.py.py
@@ -0,0 +1,10 @@
+#10. Write a program which accept name from user and display length of its name.
+
+def printlength(n):
+          j = 0
+          for i in n:
+                    j = j + 1
+          print(" Length of Name =", + j)
+
+n = input(" Enter the Name ")
+printlength(n)
